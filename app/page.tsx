8b4d60749c1bb9b27e877cfe341461f6ee53f941
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2023-12-24 12:46:18 -0800

    added dynamic data and return quizzes component

--- app/page.tsx
+++ app/page.tsx
@@ -3,41 +3,59 @@ import postgres from "postgres";
 
 const sql = postgres(process.env.POSTGRES_URL!);
 
-const quizzes = [
-    {
-      id: 1,
-      title: 'Quiz 1',
-      description: 'This is a quiz about the first quiz',
-    },
-    {
-      id: 2,
-      title: 'Quiz 2',
-      description: 'This is a quiz about the second quiz',
-    },
-    {
-      id: 3,
-      title: 'Quiz 3',
-      description: 'This is a quiz about the third quiz',
-    },
-    {
-      id: 4,
-      title: 'Quiz 4',
-      description: 'This is a quiz about the fourth quiz',
-    },
-    {
-      id: 5,
-      title: 'Quiz 5',
-      description: 'This is a quiz about the fifth quiz',
-    },
-  ];
+// Dynamic data from database(postgres)
+async function Quizzes() {
+  const quizzes = await sql`
+  SELECT * FROM quizzes
+  `;
+  console.log(quizzes);
+  return quizzes.map((quiz) => {
+    return (
+      <div key={quiz.id} className="flex flex-col items-center">
+        <Link href={`/quiz/${quiz.id}`}>
+          <p className="text-xl underline">{quiz.title}</p>
+        </Link>
+        <p className="text-lg">{quiz.description}</p>
+      </div>
+    );
+  });
+}
 
+// Static data
+// const quizzes = [
+//     {
+//       id: 1,
+//       title: 'Quiz 1',
+//       description: 'This is a quiz about the first quiz',
+//     },
+//     {
+//       id: 2,
+//       title: 'Quiz 2',
+//       description: 'This is a quiz about the second quiz',
+//     },
+//     {
+//       id: 3,
+//       title: 'Quiz 3',
+//       description: 'This is a quiz about the third quiz',
+//     },
+//     {
+//       id: 4,
+//       title: 'Quiz 4',
+//       description: 'This is a quiz about the fourth quiz',
+//     },
+//     {
+//       id: 5,
+//       title: 'Quiz 5',
+//       description: 'This is a quiz about the fifth quiz',
+//     },
+//   ];
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center text-center py-4">
       <h1 className="font-semibold text-4xl">All Quizzes</h1>
       <div className="flex flex-col items-center">
-        {quizzes.map((quiz) => {
+        {/* {quizzes.map((quiz) => {
           return (
             <div key={quiz.id} className="flex flex-col items-center">
               <Link href={`/quiz/${quiz.id}`}>
@@ -46,7 +64,8 @@ export default function Home() {
               <p className="text-lg">{quiz.description}</p>
             </div>
           );
-        })}
+        })} */}
+        <Quizzes />
       </div>
     </div>
   );
