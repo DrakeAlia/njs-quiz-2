1b06bddf7f79ab6676460d7af09f6ffc0c79c4db
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2024-01-10 15:01:43 -0800

    initial setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,72 +1,28 @@
 import Link from "next/link";
-import postgres from "postgres";
 
-const sql = postgres(process.env.POSTGRES_URL!);
-
-// Dynamic data from database(postgres)
-async function Quizzes() {
-  const quizzes = await sql`
-  SELECT * FROM quizzes
-  `;
-  console.log(quizzes);
-  return quizzes.map((quiz) => {
-    return (
-      <div key={quiz.id} className="flex flex-col items-center">
-        <Link href={`/quiz/${quiz.id}`}>
-          <p className="text-xl underline">{quiz.title}</p>
-        </Link>
-        <p className="text-lg">{quiz.description}</p>
-      </div>
-    );
-  });
+function Quizzes() {
+  return (
+    <section>
+      <ul className="underline">
+        <li>
+          <Link href="/quiz/1">Quiz 1</Link>
+        </li>
+        <li>
+          <Link href="/quiz/2">Quiz 2</Link>
+        </li>
+        <li>
+          <Link href="/quiz/3">Quiz 3</Link>
+        </li>
+      </ul>
+    </section>
+  );
 }
-
-// Static data
-// const quizzes = [
-//     {
-//       id: 1,
-//       title: 'Quiz 1',
-//       description: 'This is a quiz about the first quiz',
-//     },
-//     {
-//       id: 2,
-//       title: 'Quiz 2',
-//       description: 'This is a quiz about the second quiz',
-//     },
-//     {
-//       id: 3,
-//       title: 'Quiz 3',
-//       description: 'This is a quiz about the third quiz',
-//     },
-//     {
-//       id: 4,
-//       title: 'Quiz 4',
-//       description: 'This is a quiz about the fourth quiz',
-//     },
-//     {
-//       id: 5,
-//       title: 'Quiz 5',
-//       description: 'This is a quiz about the fifth quiz',
-//     },
-//   ];
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center text-center py-4">
-      <h1 className="font-semibold text-4xl">All Quizzes</h1>
-      <div className="flex flex-col items-center">
-        {/* {quizzes.map((quiz) => {
-          return (
-            <div key={quiz.id} className="flex flex-col items-center">
-              <Link href={`/quiz/${quiz.id}`}>
-                <p className="text-xl underline">{quiz.title}</p>
-              </Link>
-              <p className="text-lg">{quiz.description}</p>
-            </div>
-          );
-        })} */}
-        <Quizzes />
-      </div>
+    <div>
+      <h1 className="text-2xl font-bold">All Quizzes</h1>
+      <Quizzes />
     </div>
   );
 }
--- app/quiz/[id]/page.tsx
+++ app/quiz/[id]/page.tsx
@@ -1,20 +1,7 @@
-import { headers } from "next/headers";
-
-export default function QuizPage() {
-  const heads = headers();
-
-  const pathname = heads.get("next-url");
-
-  console.log(pathname);
-
-  const id = pathname?.split("/")[2];
-
-  console.log(id);
-
+export default function QuizPage({ params }: { params: { id: string } }) {
   return (
-    <div className="flex flex-col items-center text-center py-4">
-      <h1 className="font-semibold text-4xl p-12">{`Quiz ${id}`}</h1>
-      <p>This is a quiz about the first quiz</p>
+    <div>
+      <h1>Quiz {params.id}</h1>
     </div>
   );
 }
